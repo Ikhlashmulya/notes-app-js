4a67226d8de1500eb7a725408ed6268649c171b2
Ikhlashmulya <nurahmanmulya@gmail.com>
2025-02-19 16:42:22 +0700

    add jsdoc in webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
 
+/** @type {import("webpack").Configuration} */
 export default merge(common, {
   mode: "production",
   module: {
